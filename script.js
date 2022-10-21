c59c51292c6096cbcd65f00096e303048aa03e2b
Damian <mdq4all@gmail.com>
2022-10-21 13:06:12 -0300

    clase 02 complerada

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector("[data-form-btn]");
+
+const createTask = (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector("[data-form-input]");
+    console.log(input.value)
+};
+
+btn.addEventListener ("click", createTask);
