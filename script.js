17117df2a9c7d0e0284dd918adb45ba93298c486
Damian <mdq4all@gmail.com>
2022-10-21 14:04:27 -0300

    agregando tarea

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const btn = document.querySelector("[data-form-btn]");
 
 const createTask = (evento) => {
+
     evento.preventDefault();
+
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value)
+    const value = input.value;
+    const list = document.querySelector("[data-list]")
+    const tarea = document.createElement("li");
+    
+    tarea.classList.add("card");
+    input.value = "";
+    const content = `<div>
+        <i class="far fa-check-square icon"></i>
+        <span class="task">${value}</span>
+    </div>
+        <i class="fas fa-trash-alt trashIcon icon"></i>`;
+
+    tarea.innerHTML = content;
+    list.appendChild(tarea);
+    console.log(content);
 };
 
-btn.addEventListener ("click", createTask);
+btn.addEventListener("click", createTask);
